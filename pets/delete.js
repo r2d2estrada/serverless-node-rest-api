07862586a4cacfaf2f2eb55d201aa861403f1c97
Arturo Estrada rodriguez <arturoe@mty.ipointsystems.com>
2019-10-04 17:20:54 -0500

    finished API (pending tests)

---
+++ pets/delete.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.delete = (event, context, callback) => {
+
+    const params = {
+        TableName: process.env.DYNAMO_TABLE,
+        Key: {
+            id: event.pathParameters.id
+        }
+    };
+
+    dynamoDb.delete(params, (err, data) => {
+        if (err) {
+            console.error(err);
+            return callback(new Error('Unable to remove  item'));
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(params.Item)
+            });
+        }
+    });
+}
---
+++ pets/list.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.delete = (event, context, callback) => {
+
+    const params = {
+        TableName: process.env.DYNAMO_TABLE,
+    };
+
+    dynamoDb.scan(params, (err, data) => {
+        if (err) {
+            console.error(err);
+            return callback(new Error('Fetch Items'));
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(data.Item)
+            });
+        }
+    });
+}
